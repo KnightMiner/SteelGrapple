0e265f4f4124ee8c38b5e6ff05c8859a08b5fea1
KnightMiner <knightminer4@gmail.com>
2020-03-07 23:54:51 -0600

    Add judo mech improvements
    Judo mech now has the option of more range, and can target mountains to throw a rock

--- scripts/init.lua
+++ scripts/init.lua
@@ -30,12 +30,104 @@ function mod:loadScript(path)
 end
 
 function mod:metadata()
+  modApi:addGenerationOption(
+    "rockThrow",
+    "Rock Throw",
+    "If checked, judo mech is allowed to target mountains, throwing a rock instead of a unit",
+    { enabled = true }
+  )
+  modApi:addGenerationOption(
+    "judoBaseRange",
+    "Base Judo Throw Range",
+    "Judo mech's base throw range without upgrades",
+    {
+      value = 1, -- default
+      values = { 1, 2 }
+    }
+  )
+  modApi:addGenerationOption(
+    "replaceUpgrade",
+    "Judo Range Upgrade",
+    "Determines how the judo mech can upgrade range.",
+    {
+      value = "both", -- default
+      values = { "none", "ally", "damage", "both" },
+      strings = { "None", "Ally Immune", "Replace Damage", "Boost From Both Upgrades" }
+    }
+  )
 end
 
 function mod:init()
+  self:loadScript("weaponPreview/api")
+  self.modApiExt = self:loadScript("modApiExt/modApiExt")
+  self.modApiExt:init()
+  self:loadScript("skills/judo")
 end
 
 function mod:load(options,version)
+  self.modApiExt:load(self, options, version)
+  self:loadScript("weaponPreview/api"):load()
+
+  -- update from config
+  -- rock throw
+  self.rockThrow = options.rockThrow.enabled
+  local image = self.rockThrow and "Mountain" or "Normal"
+  for _, weapon in pairs({"Prime_Shift", "Prime_Shift_B"}) do
+    _G[weapon].TipImage = _G[weapon].TipImages[image]
+  end
+
+  -- judo base range, need to set all options
+  local baseRange = options.judoBaseRange.value
+  for _, weapon in pairs({
+    "Prime_Shift",
+    "Prime_Shift_A_Friendly", "Prime_Shift_A_Master",
+    "Prime_Shift_B_Damage",   "Prime_Shift_B_Range",
+    "Prime_Shift_AB_Base",    "Prime_Shift_AB_Master",
+    "Prime_Shift_AB_Range",   "Prime_Shift_AB_Both"
+  }) do
+    _G[weapon].Range = baseRange + _G[weapon].RangeBoost
+  end
+
+  -- judo range upgrade choice
+  local replaceUpgrade = options.replaceUpgrade.value
+  -- ally means we replace ally immune with judo master
+  if replaceUpgrade == "ally" then
+    Prime_Shift_A = Prime_Shift_A_Master
+    Prime_Shift_AB = Prime_Shift_AB_Master
+    Prime_Shift.UpgradeCost[1] = 2
+    Weapon_Texts.Prime_Shift_Upgrade1 = "Judo Master"
+    Weapon_Texts.Prime_Shift_A_UpgradeDescription = "Deals no damage to allies and boosts throw range by 1."
+  else
+    Prime_Shift_A = Prime_Shift_A_Friendly
+    Prime_Shift.UpgradeCost[1] = 1
+    Weapon_Texts.Prime_Shift_Upgrade1 = "Ally Immune"
+    Weapon_Texts.Prime_Shift_A_UpgradeDescription = "Deals no damage to allies."
+  end
+
+  -- damage means we replace damage with range
+  if replaceUpgrade == "damage" then
+    Prime_Shift_B = Prime_Shift_B_Range
+    Prime_Shift_AB = Prime_Shift_AB_Range
+    Weapon_Texts.Prime_Shift_Upgrade2 = "+2 Range"
+    Weapon_Texts.Prime_Shift_B_UpgradeDescription = "Increases throw range by 2."
+  else
+    Prime_Shift_B = Prime_Shift_B_Damage
+    Weapon_Texts.Prime_Shift_Upgrade2 = "+2 Damage"
+    Weapon_Texts.Prime_Shift_B_UpgradeDescription = "Increases damage by 2."
+  end
+
+  -- none is vanilla behavior
+  if replaceUpgrade == "none" then
+    Prime_Shift_AB = Prime_Shift_AB_Base
+  end
+
+  -- both gives a range boost when both are upgraded
+  if replaceUpgrade == "both" then
+    Prime_Shift_AB = Prime_Shift_AB_Both
+    Weapon_Texts.Prime_Shift_Description = "Grab a unit and toss it behind you. Apply both upgrades to increase throw range by 1."
+  else
+    Weapon_Texts.Prime_Shift_Description = "Grab a unit and toss it behind you."
+  end
 end
 
 return mod
---
+++ scripts/skills/judo.lua
@@ -0,0 +1,224 @@
+local mod = mod_loader.mods[modApi.currentMod]
+local cutils = mod:loadScript("libs/cutils")
+local previewer = mod:loadScript("weaponPreview/api")
+
+-- override Prime_Shift with changes
+Prime_Shift = Skill:new{
+  -- basic
+	Class = "Prime",
+	Icon = "weapons/prime_shift.png",
+  -- upgrades
+	PowerCost = 0,
+	Upgrades = 2,
+	UpgradeCost = {1,3},
+  -- overrides
+	Damage = 1,
+	Range = 1,
+  FriendlyDamage = true,
+	RangeBoost = 0,
+  -- display
+	LaunchSound = "/weapons/shift",
+	TipImages = {
+		Mountain = {
+			Unit          = Point(2,2),
+			Enemy         = Point(2,1),
+			Target        = Point(2,1),
+			Mountain      = Point(1,2),
+			Second_Origin = Point(2,2),
+			Second_Target = Point(1,2)
+		},
+		Normal = {
+			Unit   = Point(2,2),
+			Enemy  = Point(2,1),
+			Target = Point(2,1)
+		}
+	}
+}
+Prime_Shift.TipImage = Prime_Shift.TipImage.Mountain
+
+-- Upgrade 1a: ally immune
+Prime_Shift_A_Friendly = Prime_Shift:new{
+	FriendlyDamage = false,
+	TipImage = {
+		Unit          = Point(2,2),
+		Friendly      = Point(2,1),
+		Enemy         = Point(3,2),
+		Target        = Point(2,1),
+		Second_Origin = Point(2,2),
+		Second_Target = Point(1,2),
+	}
+}
+-- Upgrade 1b: ally immune with range
+Prime_Shift_A_Master = Prime_Shift:new{
+	FriendlyDamage = false,
+	Range = 2,
+	RangeBoost = 1,
+	TipImage = {
+		Unit          = Point(2,2),
+		Friendly      = Point(2,1),
+		Enemy         = Point(3,2),
+		Target        = Point(2,1),
+		Second_Origin = Point(2,2),
+		Second_Target = Point(0,2),
+	}
+}
+
+-- Upgrade 2a: damage
+Prime_Shift_B_Damage = Prime_Shift:new{
+	Damage = 3
+}
+-- Upgrade 2b: range
+Prime_Shift_B_Range = Prime_Shift:new{
+	Range = 3,
+	RangeBoost = 2,
+	TipImages = {
+		Mountain = {
+			Unit          = Point(2,2),
+			Enemy         = Point(2,1),
+			Target        = Point(2,4),
+			Mountain      = Point(1,2),
+			Second_Origin = Point(2,2),
+			Second_Target = Point(1,2)
+		},
+		Normal = {
+			Unit   = Point(2,2),
+			Enemy  = Point(2,1),
+			Target = Point(2,4)
+		}
+	}
+}
+
+-- Both upgrades: vanilla
+Prime_Shift_AB_Base = Prime_Shift_A_Friendly:new{
+	Damage = 3
+}
+-- both upgrades: master
+Prime_Shift_AB_Master = Prime_Shift_A_Master:new{
+	Damage = 3
+}
+-- both upgraes: range
+Prime_Shift_AB_Range = Prime_Shift_A_Master:new{
+	Range = 3,
+	RangeBoost = 2
+}
+-- both upgrades: both boost
+Prime_Shift_AB_Both = Prime_Shift_A_Master:new{
+	Damage = 3,
+	Range = 2,
+	RangeBoost = 1
+}
+
+-- default upgrades: both boosts range
+Prime_Shift_A = Prime_Shift_A_Friendly
+Prime_Shift_B = Prime_Shift_B_Damage
+Prime_Shift_AB = Prime_Shift_AB_Both
+
+-- targets landing instead of units
+function Prime_Shift:GetTargetArea(point)
+	local ret = PointList()
+	for dir = DIR_START, DIR_END do
+    local side = DIR_VECTORS[dir]
+    local target = point + side
+    -- can target non-guarding pawns or mountains
+    if (Board:IsPawnSpace(target) and not Board:GetPawn(target):IsGuarding())
+        or mod.rockThrow and Board:GetTerrain(target) == TERRAIN_MOUNTAIN then
+      -- can land on spaces behind the mech that are open
+			local canTarget = false
+      for i = 1, self.Range do
+        local landing = point - side * i
+        if not Board:IsBlocked(landing, PATH_FLYER) then
+          ret:push_back(landing)
+					canTarget = true
+        end
+      end
+			-- add the pawn as targetable too, adds compat with old behavior
+			if canTarget then
+				ret:push_back(target)
+			end
+    end
+	end
+
+	return ret
+end
+
+--[[--
+  Spawns in a rock on a mountain, as vanilla does not like spawning units on mountains
+
+  @param  space  Point to place the rock
+]]
+function Prime_Shift:AddRock(space)
+  -- start by removing the mountain
+  local mountainHealth = 0
+  if Board:GetTerrain(space) == TERRAIN_MOUNTAIN then
+    mountainHealth = cutils.GetTileHealth(Board, space)
+    Board:SetTerrain(space, TERRAIN_RUBBLE)
+  end
+
+  -- spawn in the rock
+  local rock = SpaceDamage(space, 0)
+  rock.sPawn = "RockThrown"
+  Board:DamageSpace(rock)
+
+  -- then add the mountain back if we had one
+  if mountainHealth > 0 then
+    Board:SetTerrain(space, TERRAIN_MOUNTAIN)
+    cutils.SetTileHealth(Board, space, mountainHealth)
+  end
+end
+
+-- toss units to landing
+function Prime_Shift:GetSkillEffect(p1, p2)
+	local ret = SkillEffect()
+	local dir = GetDirection(p2 - p1)
+
+	-- determine target and landing
+	local target
+	local landing = p2
+
+	-- if targeting the pawn, throw to first available space
+	if Board:IsPawnSpace(p2) or Board:GetTerrain(p2) == TERRAIN_MOUNTAIN then
+		target = p2
+		local offset = DIR_VECTORS[dir]
+		for i = 1, self.Range do
+			local point = p1 - offset * i
+			if not Board:IsBlocked(point, PATH_FLYER) then
+				landing = point
+				break
+			end
+		end
+	else
+		-- if targeting an empty space, that is where the unit lands
+		target = p1 - DIR_VECTORS[dir]
+	end
+
+	-- area to toss unit
+	local move = PointList()
+	move:push_back(target)
+	move:push_back(landing)
+
+	-- mountains throw a rock
+	if mod.rockThrow and Board:GetTerrain(target) == TERRAIN_MOUNTAIN then
+		ret:AddMelee(p1, SpaceDamage(target, self.Damage))
+		ret:AddScript(string.format("Prime_Shift:AddRock(%s)", target:GetString()))
+		ret:AddLeap(move, FULL_DELAY)
+		ret:AddBounce(landing, 3)
+
+		-- add a fake rock for the preview
+		local fakeRock = SpaceDamage(landing, 0)
+		fakeRock.sPawn = "RockThrown"
+		previewer:AddDamage(fakeRock)
+	else
+		-- animation punch and toss
+		local fake_punch = SpaceDamage(target, 0)
+		ret:AddMelee(p1, fake_punch)
+		ret:AddLeap(move, FULL_DELAY)
+
+		-- damage the target after landing
+		if self.FriendlyDamage or not Board:IsPawnTeam(target, TEAM_PLAYER) then
+			ret:AddDamage(SpaceDamage(landing, self.Damage))
+		end
+		ret:AddBounce(landing, 3)
+	end
+
+	return ret
+end
