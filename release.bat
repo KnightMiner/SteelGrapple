38a54419947419018b96b8df63f2e3c6909e2798
KnightMiner <knightminer4@gmail.com>
2020-03-07 23:53:35 -0600

    Base mod structure

---
+++ release.bat
@@ -0,0 +1,16 @@
+@echo off
+
+IF EXIST build RMDIR /q /s build
+IF EXIST SteelGrapple-#.#.#.zip" DEL SteelGrapple-#.#.#.zip"
+MKDIR build
+MKDIR build\PerfectedIslandReward
+
+REM Copy required files into build directory
+XCOPY img build\SteelGrapple\img /s /e /i
+XCOPY scripts build\SteelGrapple\scripts /s /e /i
+
+REM Zipping contents
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('build', 'SteelGrapple-#.#.#.zip'); }"
+
+REM Removing build directory
+RMDIR /q /s build
